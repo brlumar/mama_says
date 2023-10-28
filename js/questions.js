8678897f859023ee1a8b4c29e99c2bd571acaa98
brlumar <126432304+brlumar@users.noreply.github.com>
2023-10-28 16:59:48 -0500

    added structure for site and questions

---
+++ js/questions.js
@@ -0,0 +1,34 @@
+const cardData = [
+    {
+        phrase: 'As a mother, I never leave home without ______.',
+        answers: [
+            {
+                blank: 'b____',
+                answer: 'baby'
+            },
+            {
+                blank: 'b____ w____',
+                answer: 'baby whips'
+            },
+            {
+                blank: 'b____ f____',
+                answer: 'baby food'
+            }, {
+                blank: 'p______',
+                answer: 'pacifier'
+            }, {
+                blank: 'd______',
+                answer: 'diaper'
+            }, {
+                blank: 'a________',
+                answer: 'aspirator'
+            }, {
+                blank: 'b____ p___',
+                answer: 'breast pump'
+            },
+
+        ]
+    }
+]
+
+
