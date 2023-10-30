ad2194546f1ff75e38f1ff42b64ca4805652c72e
brlumar <126432304+brlumar@users.noreply.github.com>
2023-10-29 19:16:10 -0500

    added more questions and made them generate in order instead of randomly

--- js/questions.js
+++ js/questions.js
@@ -88,7 +88,482 @@ const cardData = [
             },
 
         ]
+    },
+    {
+        phrase: "Babies always make me smile when they ______.",
+        answers: [
+            {
+                blank: "s______",
+                answer: "smile"
+            },
+            {
+                blank: "l______",
+                answer: "laugh"
+            },
+            {
+                blank: "c____",
+                answer: "coo"
+            },
+            {
+                blank: "g___",
+                answer: "giggle"
+            },
+            {
+                blank: "c______",
+                answer: "cry"
+            },
+            {
+                blank: "s______",
+                answer: "sleep"
+            },
+            {
+                blank: "r_____",
+                answer: "reach"
+            }
+        ]
+    },
+    {
+        phrase: "Every baby has a unique ______ that makes them special.",
+        answers: [
+            {
+                blank: "s_____",
+                answer: "smile"
+            },
+            {
+                blank: "l____",
+                answer: "laugh"
+            },
+            {
+                blank: "p____",
+                answer: "personality"
+            },
+            {
+                blank: "c______",
+                answer: "cry"
+            },
+            {
+                blank: "s____",
+                answer: "scent"
+            },
+            {
+                blank: "s______",
+                answer: "sound"
+            },
+            {
+                blank: "f____",
+                answer: "feature"
+            }
+        ]
+    },
+    {
+        phrase: "A mother's love is as warm as a ______.",
+        answers: [
+            {
+                blank: "b____",
+                answer: "blanket"
+            },
+            {
+                blank: "s_____",
+                answer: "sunshine"
+            },
+            {
+                blank: "c______",
+                answer: "cup of cocoa"
+            },
+            {
+                blank: "h____",
+                answer: "hug"
+            },
+            {
+                blank: "c____",
+                answer: "cup of tea"
+            },
+            {
+                blank: "f____",
+                answer: "fire"
+            },
+            {
+                blank: "s______",
+                answer: "summer day"
+            }
+        ]
+    },
+    {
+        phrase: "A baby's first word is usually ______.",
+        answers: [
+            {
+                blank: "m______",
+                answer: "mama"
+            },
+            {
+                blank: "d______",
+                answer: "dada"
+            },
+            {
+                blank: "b_____",
+                answer: "baba"
+            },
+            {
+                blank: "n____",
+                answer: "no"
+            },
+            {
+                blank: "y____",
+                answer: "yes"
+            },
+            {
+                blank: "l____",
+                answer: "love"
+            },
+            {
+                blank: "s______",
+                answer: "sleep"
+            }
+        ]
+    },
+    {
+        phrase: "Mothers cherish the first time their baby ______.",
+        answers: [
+            {
+                blank: "s______",
+                answer: "smiles"
+            },
+            {
+                blank: "l______",
+                answer: "laughs"
+            },
+            {
+                blank: "c____",
+                answer: "crawls"
+            },
+            {
+                blank: "w____",
+                answer: "walks"
+            },
+            {
+                blank: "s______",
+                answer: "says 'mama'"
+            },
+            {
+                blank: "h_____",
+                answer: "holds their finger"
+            },
+            {
+                blank: "f____",
+                answer: "falls asleep in their arms"
+            }
+        ]
+    },
+    {
+        phrase: "Babies have a special way of bringing ______ into a mother's life.",
+        answers: [
+            {
+                blank: "j____",
+                answer: "joy"
+            },
+            {
+                blank: "l____",
+                answer: "love"
+            },
+            {
+                blank: "l______",
+                answer: "laughter"
+            },
+            {
+                blank: "h_____",
+                answer: "hope"
+            },
+            {
+                blank: "m_____",
+                answer: "magic"
+            },
+            {
+                blank: "a____",
+                answer: "adventure"
+            },
+            {
+                blank: "c____",
+                answer: "contentment"
+            }
+        ]
+    },
+    {
+        phrase: "A mother's intuition is as strong as a ______.",
+        answers: [
+            {
+                blank: "b_____",
+                answer: "bear"
+            },
+            {
+                blank: "h______",
+                answer: "hawk"
+            },
+            {
+                blank: "t______",
+                answer: "tiger"
+            },
+            {
+                blank: "l______",
+                answer: "lioness"
+            },
+            {
+                blank: "w______",
+                answer: "wolf"
+            },
+            {
+                blank: "e____",
+                answer: "elephant"
+            },
+            {
+                blank: "s______",
+                answer: "superhero"
+            }
+        ]
+    },
+    {
+        phrase: "A mother's lap is the comfiest place for a baby to ______.",
+        answers: [
+            {
+                blank: "r____",
+                answer: "rest"
+            },
+            {
+                blank: "s______",
+                answer: "snuggle"
+            },
+            {
+                blank: "c______",
+                answer: "cuddle"
+            },
+            {
+                blank: "s____",
+                answer: "sleep"
+            },
+            {
+                blank: "p_____",
+                answer: "play"
+            },
+            {
+                blank: "g____",
+                answer: "giggle"
+            },
+            {
+                blank: "d_____",
+                answer: "dream"
+            }
+        ]
+    },
+    {
+        phrase: "A baby's laughter is the most delightful sound in the ______.",
+        answers: [
+            {
+                blank: "w____",
+                answer: "world"
+            },
+            {
+                blank: "u______",
+                answer: "universe"
+            },
+            {
+                blank: "r____",
+                answer: "room"
+            },
+            {
+                blank: "a____",
+                answer: "air"
+            },
+            {
+                blank: "h____",
+                answer: "house"
+            },
+            {
+                blank: "f______",
+                answer: "forest"
+            },
+            {
+                blank: "g______",
+                answer: "garden"
+            }
+        ]
+    },
+    {
+        phrase: "A mother's heart is full of endless ______ for her baby.",
+        answers: [
+            {
+                blank: "l____",
+                answer: "love"
+            },
+            {
+                blank: "j____",
+                answer: "joy"
+            },
+            {
+                blank: "c____",
+                answer: "care"
+            },
+            {
+                blank: "p____",
+                answer: "pride"
+            },
+            {
+                blank: "h_____",
+                answer: "hope"
+            },
+            {
+                blank: "t______",
+                answer: "tenderness"
+            },
+            {
+                blank: "p______",
+                answer: "patience"
+            }
+        ]
+    },
+    {
+        phrase: "Babies have an incredible ability to make even the toughest days ______.",
+        answers: [
+            {
+                blank: "b______",
+                answer: "bright"
+            },
+            {
+                blank: "h______",
+                answer: "hopeful"
+            },
+            {
+                blank: "s_____",
+                answer: "special"
+            },
+            {
+                blank: "m______",
+                answer: "meaningful"
+            },
+            {
+                blank: "j____",
+                answer: "joyful"
+            },
+            {
+                blank: "l______",
+                answer: "lovely"
+            },
+            {
+                blank: "w______",
+                answer: "wonderful"
+            }
+        ]
+    },
+    {
+        phrase: "A mother's hands are skilled at soothing a baby's ______.",
+        answers: [
+            {
+                blank: "c_____",
+                answer: "cries"
+            },
+            {
+                blank: "f_____",
+                answer: "fears"
+            },
+            {
+                blank: "t______",
+                answer: "tears"
+            },
+            {
+                blank: "s______",
+                answer: "sorrows"
+            },
+            {
+                blank: "p______",
+                answer: "pain"
+            },
+            {
+                blank: "w______",
+                answer: "worries"
+            },
+            {
+                blank: "t______",
+                answer: "tummy"
+            }
+        ]
+    },
+    {
+        phrase: "A baby's cuddle can make a mother's heart ______.",
+        answers: [
+            {
+                blank: "s______",
+                answer: "sing"
+            },
+            {
+                blank: "d______",
+                answer: "dance"
+            },
+            {
+                blank: "s______",
+                answer: "smile"
+            },
+            {
+                blank: "l______",
+                answer: "leap"
+            },
+            {
+                blank: "s______",
+                answer: "soar"
+            },
+            {
+                blank: "g______",
+                answer: "glow"
+            },
+            {
+                blank: "w______",
+                answer: "warm"
+            }
+        ]
+    },
+    {
+        phrase: "A mother's prayers are like a protective ______ around her baby.",
+        answers: [
+            {
+                blank: "b____",
+                answer: "bubble"
+            },
+            {
+                blank: "s____",
+                answer: "shield"
+            },
+            {
+                blank: "a______",
+                answer: "armor"
+            },
+            {
+                blank: "w___",
+                answer: "wall"
+            },
+            {
+                blank: "c_____",
+                answer: "cloak"
+            },
+            {
+                blank: "g______",
+                answer: "guard"
+            },
+            {
+                blank: "h______",
+                answer: "halo"
+            }
+        ]
     }
 ]
+    
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- js/script.js
+++ js/script.js
@@ -16,8 +16,11 @@ let time = 0; //seconds allowed per turn
 let timerInterval; //variable for the clock interval
 let whoseTurn = 1;
 
+let index = -1;
 
-let currentCardIndex = Math.floor(Math.random() * cardData.length); //index that indicates which card is currently being displayed
+// let currentCardIndex = Math.floor(Math.random() * cardData.length); //index that indicates which card is currently being displayed
+let currentQuestion = cardData[index];
+let correctAnswers = 0;
 
 
 
@@ -27,6 +30,8 @@ function startTimer() {
 
     if (whoseTurn == 1) {
         if (time == 0) {
+            index++;
+            currentQuestion = cardData[index];
             clearCard();
             updateQuestion();
             time = 30; //resets the timer to 60 seconds
@@ -39,7 +44,10 @@ function startTimer() {
         }
     } else {
         if (time == 0) {
-
+            index++;
+            currentQuestion = cardData[index];
+            clearCard();
+            updateQuestion();
             time = 30; //resets the time to 60 seconds 
             clearInterval(timerInterval); //temperarily keeps the timer interval from doubling up. disabling the button while time is running with fix the issue permantantly
             // showCard();
@@ -58,9 +66,10 @@ function startTimer() {
 //fuction that updates the time and subtract and extra second afer wrong answer
 function updateTimer() {
     timeElement.innerText = time;
-    if (time <= 0) {
+    if (time <= 0 || correctAnswers == 7) {
         clearInterval(timerInterval);
-
+        correctAnswers = 0;
+        time=0;
         if (whoseTurn == 1) {
             whoseTurn++;
         } else if (whoseTurn == 2) {
@@ -74,15 +83,14 @@ function updateTimer() {
 
 function updateQuestion() {
 
-const currentQuestion = cardData[currentCardIndex];
-question.innerHTML = currentQuestion.phrase;
-answer1.innerHTML = currentQuestion.answers[0].blank;
-answer2.innerHTML = currentQuestion.answers[1].blank;
-answer3.innerHTML = currentQuestion.answers[2].blank;
-answer4.innerHTML = currentQuestion.answers[3].blank;
-answer5.innerHTML = currentQuestion.answers[4].blank;
-answer6.innerHTML = currentQuestion.answers[5].blank;
-answer7.innerHTML = currentQuestion.answers[6].blank;
+    question.innerHTML = currentQuestion.phrase;
+    answer1.innerHTML = currentQuestion.answers[0].blank;
+    answer2.innerHTML = currentQuestion.answers[1].blank;
+    answer3.innerHTML = currentQuestion.answers[2].blank;
+    answer4.innerHTML = currentQuestion.answers[3].blank;
+    answer5.innerHTML = currentQuestion.answers[4].blank;
+    answer6.innerHTML = currentQuestion.answers[5].blank;
+    answer7.innerHTML = currentQuestion.answers[6].blank;
 
 
 }
@@ -100,4 +108,43 @@ function clearCard() {
 }
 
 
+function answer1Correct() {
+    answer1.innerHTML = currentQuestion.answers[0].answer;
+correctAnswers++;
+    console.log("questions one correct");
+}
+
+function answer2Correct() {
+    answer2.innerHTML = currentQuestion.answers[1].answer;
+correctAnswers++;
+}
+function answer3Correct() {
+    answer3.innerHTML = currentQuestion.answers[2].answer;
+correctAnswers++;
+}
+function answer4Correct() {
+    answer4.innerHTML = currentQuestion.answers[3].answer;
+correctAnswers++;
+}
+function answer5Correct() {
+    answer5.innerHTML = currentQuestion.answers[4].answer;
+correctAnswers++;
+}
+function answer6Correct() {
+    answer6.innerHTML = currentQuestion.answers[5].answer;
+correctAnswers++;
+}
+function answer7Correct() {
+    answer7.innerHTML = currentQuestion.answers[6].answer;
+correctAnswers++;
+}
+
+
 startBtn.addEventListener('click', startTimer);
+answer1.addEventListener('click', answer1Correct);
+answer2.addEventListener('click', answer2Correct);
+answer3.addEventListener('click', answer3Correct);
+answer4.addEventListener('click', answer4Correct);
+answer5.addEventListener('click', answer5Correct);
+answer6.addEventListener('click', answer6Correct);
+answer7.addEventListener('click', answer7Correct);
